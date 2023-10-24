f18c4f36d1c742b435325547a7d193c76cc3ce87
Austin Conn <austinc3030@gmail.com>
2023-10-24 13:20:57 -0400

    Update script to create library

--- ExportGDT.py
+++ ExportGDT.py
@@ -1,24 +1,28 @@
-# Exports a Ghidra Data Type (GDT) archive for the current program
-# @author 13laze
+# Creates or Adds data to a Ghidra Data Type (GDT) archive for the current program
+# @author 13laze/austinc3030
 # @menupath Tools.Data Types.ExportGDT
 # @category Data Types
+
+import os
 
 from ghidra.util import Msg
 from ghidra.program.model.data import FileDataTypeManager
 from ghidra.app.script import GhidraScript
 from ghidra.app.cmd.function import CaptureFunctionDataTypesCmd
-from docking.widgets.filechooser import GhidraFileChooser
 from ghidra.util.task import TaskMonitor
 
-chooser = GhidraFileChooser(None)
 
-chooser.setTitle("Choose GDT Archive Path")
-chooser.setApproveButtonText("Save As");
-chooser.setMultiSelectionEnabled(False);
 
-f = chooser.getSelectedFile(True)
+# For exporting a ghidra data type archive for the current program,
+# specify a new file that does not exist. For adding to an existing
+# ghidra data type archive (creating a library of sorts), specify an
+# existing ghidra data type archive.
+gdt = askFile('GDT Archive File', 'OK').path
 
-dtm = FileDataTypeManager.createFileArchive(f)
+if os.path.exists(gdt):
+    dtm = FileDataTypeManager.openFileArchive(gdt, True)
+else:
+    dtm = FileDataTypeManager.createFileArchive(gdt)
 cmd = CaptureFunctionDataTypesCmd(dtm, currentProgram.getMemory(), None)
 cmd.applyTo(currentProgram, TaskMonitor.DUMMY)
 
